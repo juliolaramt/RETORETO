2ff2ca0057d2af61c93d4626bbce0e0e8da60503
juliolaramt <julio.lara.mt@usa.edu>
2022-10-08 12:28:48 -0500

    nombr del commit

---
+++ src/main/java/com/Master/Ciclo3/RetoMasterApplication.java
@@ -0,0 +1,13 @@
+package com.Master.Ciclo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetoMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetoMasterApplication.class, args);
+	}
+
+}
